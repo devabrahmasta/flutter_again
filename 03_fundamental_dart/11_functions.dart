dba64e223fc5ea891b246a6b8d5690bef094641a
Deva Brahmasta <devabrahmasta24@gmail.com>
2025-08-05 18:45:14 +0800

    11 function

---
+++ 03_fundamental_dart/11_functions.dart
@@ -0,0 +1,5 @@
+// Function itu digunakan untuk menghasilkan output berdasarkan input tertentu
+// juga sebagai prosedur yang dapat dipakai berulang kali.
+// contohnya seperti main(), print() yang selalu kita pakai.
+
+// fungsi print akan mengambil nilai dan menampilkannya ke konsol (terminal)

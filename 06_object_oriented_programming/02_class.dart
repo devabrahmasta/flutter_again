785ce02d3082760ce08e3b649f6dcf4c90cf9a54
Deva Brahmasta <devabrahmasta24@gmail.com>
2025-08-18 14:07:37 +0700

    f6: class oop

---
+++ 06_object_oriented_programming/02_class.dart
@@ -0,0 +1,12 @@
+// Class
+// class adalah blueprint untuk membuat object
+// di dalam kelas kita bisa mendefinisakn sifat(attribute) dan perilaku(behaviour) dari objek yang mau dibuat
+// sebagai contoh kelas dengan nama animal
+// Animal
+// + String name
+// + int age
+// + double weight
+
+// - eat()
+// - sleep()
+// - poop()
